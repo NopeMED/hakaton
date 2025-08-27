5a00aba69c7e9e4e46f63d986716caa84629a096
ROMI <broromi30@gmail.com>
2025-08-27 16:55:15 +0300

    xzcvbn

--- main.py
+++ main.py
@@ -8,9 +8,33 @@ root.geometry('1600x900')
 
 c = Canvas(root, bg = "#e8dab2", width=1600, height=900)
 person = PhotoImage(file="person.PNG")
-image_label = tkinter.Label(root, image=person)
-image_label.pack()
+new_image = person.subsample(2, 2)
+image_label = tkinter.Label(root, image=new_image)
+
 c.pack()
+
+text_var = tkinter.StringVar()
+text_var.set("Name\nLastname")
+label = tkinter.Label(c,
+                 textvariable=text_var,
+                 anchor=tkinter.W,
+                 bg="#e8dab2",
+                 height=3,
+                 width=30,
+                 bd=3,
+                 font=("Arial", 16, "bold"),
+                 cursor="hand2",
+                 fg="black",
+                 padx=15,
+                 pady=15,
+                 wraplength=250
+                )
+
+
+label.pack()
+image_label.pack()
+
+
 
 
 
